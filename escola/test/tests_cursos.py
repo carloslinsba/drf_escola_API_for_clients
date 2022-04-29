4634974d80429f722237e6f499cc35022d4a1ce7
carloslinsba <carloslinsba@gmail.com>
2022-04-29 16:24:05 -0300

    ident correction at test_cursos.

--- escola/test/tests_cursos.py
+++ escola/test/tests_cursos.py
@@ -42,9 +42,9 @@ class CursoTestCase(APITestCase):
         response = self.client.put('/cursos/1/', data = data)
         self.assertEquals(response.status_code, status.HTTP_200_OK)
 
-        def test_requisicao_delete_para_deletar_cursos(self):
-            """testing delete not permitted and its response"""
-            response = self.client.delete('/cursos/1/')
-            self.assertEquals(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
-            
+    def test_requisicao_delete_para_deletar_cursos(self):
+        """testing delete not permitted and its response"""
+        response = self.client.delete('/cursos/1/')
+        self.assertEquals(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+        
     
